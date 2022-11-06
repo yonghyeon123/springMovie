22daf55d13768dfe4fe5dfce791c9fe9c8ea968b
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-06 23:04:34 +0900

    Initial commit

---
+++ src/main/java/com/aia/springMovie/SpringMovieApplication.java
@@ -0,0 +1,13 @@
+package com.aia.springMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aia/springMovie/model/UserDTO.java
@@ -0,0 +1,11 @@
+package com.aia.springMovie.model;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private int id;
+    private String username;
+    private String password;
+    private String nickname;
+}
